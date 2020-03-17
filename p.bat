619c11a8ca4d1568910b94b3381efa75e33fd127
xbl3 <58650872+xbl3@users.noreply.github.com>
2020-03-17 18:24:55 -0400

    Sauce / origination
    what made top of the line for the sauce

---
+++ p.bat
@@ -0,0 +1,19 @@
+REM This is what started the topofline.bat - one fucking thing always leads to another
+
+
+
+@echo off&set "ext=%~x0"&set "ibe=%0"&set "all=%*"&set "one=%~1"&set "two=%~2"&set "three=%~3"&set "four=%~4"&set "five=%~5"&set "six=%~6"&set "seven=%~7"&set "eight=%~8"&set "nine=%~9"&set "kvp12=%~1 %~2"&set "kvp23=%~2 %~3"&set "kvp34=%~3 %~4" &set "kvp45=%~4 %~5"&set "kvp56=%~5 %~6"&set "kvp67=%~6 %~7"&set "kvp78=%~7 %~8"&set "kvp89=%~8 %~9"&set "seq~29=%~2 %~3 %~4 %~5 %~6 %~7 %~8 %~9"&set "t2_t9=%~2 %~3 %~4 %~5 %~6 %~7 %~8 %~9"&set "q2_q9="%~2" "%~3" "%~4" "%~5" "%~6" "%~7" "%~8" "%~9""
+set "go=&goto:eof"
+set "eo=echo:"
+set "e%%=%%e%%echo:"
+set "e1=%e1%&echo:"
+echo:%e
+%e 54564565656313564 asfkldjskdshkj gkj hkjs hkjds jkhkjdshkjud kljd
+echo:%t2_t9%
+echo:%q2_q9%
+%eo%as54f4as56f4  56 456g6 d4s564d56gdg45d456d465dg4
+goto:eof
+
+echo:%seq~29%&echo:&for %%i in (1,1,3) do (echo:)
+echo/%one%&echo\%two%
+echo %%~x0%
